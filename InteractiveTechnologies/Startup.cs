0ccfc45ec38bef9edc289d44d7fa277288696ced
Stephen <sore@ideasthatfloat.com>
2018-12-11 10:02:35 -0600

    Added Identity and styled quite a bit of stuff.

---
+++ InteractiveTechnologies/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace InteractiveTechnologies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
