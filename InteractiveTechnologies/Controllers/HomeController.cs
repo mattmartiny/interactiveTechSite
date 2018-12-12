d7319e9b1fe1f4ccea4942693125c21a4a6cabc4
Stephen <sore@ideasthatfloat.com>
2018-12-12 14:52:49 -0600

    Added Contact form

--- InteractiveTechnologies/Controllers/HomeController.cs
+++ InteractiveTechnologies/Controllers/HomeController.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net.Mail;
 using System.Web.Mvc;
 using InteractiveTechnologies.Models;
-
+using System.Net;
 
 namespace InteractiveTechnologies.Controllers
 {
@@ -22,13 +23,54 @@ namespace InteractiveTechnologies.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contact(ContactViewModel contact)
         {
-            ViewBag.Message = "Your contact page.";
+            // Create the body of the email
+            string body = $"Name: {contact.Name} <br/>Email: {contact.Email}<br/>Subject: {contact.Subject}<br/>Message: {contact.Message}";
 
+
+            //Create and configure the MailMessage object
+            //Must add using system.Net.Mail
+            MailMessage msg = new MailMessage("no-reply@mattmartiny.com", "mattmartiny@yahoo.com", contact.Subject + " - " + DateTime.Now, body);
+            //Additional properties for Mail MEssage objects
+            msg.IsBodyHtml = true;
+            msg.Priority = MailPriority.Normal;
+
+
+            //Create and configure the smtpClient object
+            SmtpClient client = new SmtpClient("mail.mattmartiny.com");
+            client.Credentials =
+                new NetworkCredential("no-reply@mattmartiny.com", "10316CodySt.");
+            //Attempt to send the email
+
+            if (ModelState.IsValid)
+            {
+
+                try
+                {
+
+                    client.Send(msg);
+
+                }
+                catch (Exception ex)
+                {
+                    //for testing purposes should not be included in production
+                    //ViewBag.ExceptionMessage = ex.InnerException;
+
+                    //Error message 
+                    ViewBag.ErrorMessage = "Unable to send email.  Please try again";
+                    return View();
+                }
+
+                //if message was sent, send user to ContactConfirmation View and display info from the message
+
+                return View("ContactConfirmation", contact);
+            }
             return View();
         }
-       
+
+          
+
         [Authorize]
         public ActionResult MembersOnly()
         {                       
---
+++ InteractiveTechnologies/Models/ContactViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InteractiveTechnologies.Models
+{
+    public class ContactViewModel
+    {
+
+      
+            [Required(ErrorMessage = "**Name is Required")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "**Email is Required")]
+            [EmailAddress(ErrorMessage = "*Enter a valid Email Address*")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "**Subject is Required")]
+            public string Subject { get; set; }
+
+            [Required(ErrorMessage = "**Message is Required")]
+            public string Message { get; set; }
+    
+
+    }
+}
