56f8afe7355e79170cf2da69e6fe54f5bdeae9e4
Stephen <sore@ideasthatfloat.com>
2019-01-08 14:22:37 -0600

    added metadata to slideshow information

---
+++ InteractiveTechnologies/Models/Metadata/SlideshowMetadata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InteractiveTechnologies.Models//.Metadata
+{
+    public class SlideshowMetadata
+    {
+        public int SlideID { get; set; }
+
+        [Display(Name = "Image")]
+        public int ImageID {get; set; }
+        [Display(Name = "Slide Link")]
+        public string SlideLink { get; set; }
+        public bool Display { get; set; }
+        [Display(Name = "Slide Order")]
+        public int SlideOrder { get; set; }
+
+
+    }
+      [MetadataType(typeof(SlideshowMetadata))]
+    public partial class Slideshow{ }
+
+}
